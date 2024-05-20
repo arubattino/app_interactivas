dee8c0b8a22e84e8852b36e0335731f3ff7dcd36
arubattino <ajrubattino@gmail.com>
2024-05-19 21:39:28 -0300

    Primera version demo

---
+++ app.js
@@ -0,0 +1,85 @@
+const mongoose = require('mongoose');
+
+// Conectar a la base de datos
+mongoose.connect('mongodb://localhost:27017/usuarios', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Conectado a MongoDB');
+    // Llamar a la función para eliminar todos los usuarios
+    eliminarTodosLosUsuarios()
+        .then(() => {
+            // Llamar a la función para imprimir todos los usuarios después de eliminarlos
+            //return imprimirTodosLosUsuarios();
+        }).then(() => {
+            // Crear y guardar un nuevo documento después de eliminar todos los usuarios
+            //return guardarNuevoUsuarios();
+        }).then(() => {
+            // Imprimir todos los usuarios después de guardar uno nuevo
+            //return imprimirTodosLosUsuarios();
+        }).catch((error) => {
+            console.error('Error:', error);
+        });
+    eliminarTodosLosUsuarios()
+}).catch((error) => {
+    console.error('Error al conectar a MongoDB:', error);
+});
+
+// Definir un esquema
+const Schema = mongoose.Schema;
+const ejemploSchema = new Schema({
+    nombre: { type: String, required: true },
+    apellido: { type: String, required: true },
+    mascota: { type: String, required: true },
+    edad_mascota: { type: Number, min: 0 },
+    activo: { type: Boolean, default: true },
+    mail: {
+        type: String,
+        required: true,
+        match: [/.+\@.+\..+/, 'Por favor ingrese un correo válido']
+    }
+});
+
+// Crear un modelo
+const Ejemplo = mongoose.model('Ejemplo', ejemploSchema);
+
+// Función para eliminar todos los usuarios
+function eliminarTodosLosUsuarios() {
+    return Ejemplo.deleteMany({})
+        .then(() => {
+            console.log('Todos los usuarios han sido eliminados.');
+        })
+        .catch((error) => {
+            console.error('Error al eliminar usuarios:', error);
+        });
+}
+
+// Función para imprimir todos los usuarios en la base de datos
+function imprimirTodosLosUsuarios() {
+    return Ejemplo.find()
+        .then((docs) => {
+            console.log('Usuarios encontrados:', docs);
+        })
+        .catch((error) => {
+            console.error('Error al consultar usuarios:', error);
+        });
+}
+
+// Función para crear y guardar un nuevo usuario
+function guardarNuevoUsuarios() {
+    const nuevoUsuario = new Ejemplo({
+        nombre: 'Juan Carlos',
+        apellido: 'Sanchez',
+        mascota: 'Perro',
+        edad_mascota: 3,
+        mail: 'juan.perez@example.com'
+    });
+
+    return nuevoUsuario.save()
+        .then((doc) => {
+            console.log('Usuario creado con éxito:', doc);
+        })
+        .catch((error) => {
+            console.error('Error al crear usuario:', error);
+        });
+}
